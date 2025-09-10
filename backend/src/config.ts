3c297dcea7f8082c300ae73eb1ade457ba5847b8
Paulo Moraes <accounts@paulomoraes.dev>
2025-09-10 09:03:51 -0300

    feat: add support to multiform data

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -13,4 +13,13 @@ export const config = {
   port: Number(process.env.API_PORT) || 3000,
   apiToken: process.env.API_TOKEN || 'default-token',
   corsOrigin: process.env.CORS_ORIGIN || '*',
+  uploadSetting: {
+    fieldNameSize: 100,         // Max field name size in bytes
+    fieldSize: 100,             // Max field value size in bytes
+    fields: 10,                 // Max number of non-file fields
+    fileSize: 5 * 1024 * 1024,  // For multipart forms, the max file size in bytes
+    files: 1,                   // Max number of file fields
+    headerPairs: 2000,          // Max number of header key=>value pairs
+    parts: 1000                 // For multipart forms, the max number of parts (fields + files)
+  }
 };
